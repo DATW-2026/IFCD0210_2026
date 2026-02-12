b9175571674009bc165893e4fbd3af40490da50e
Alejandro Cerezo <alce65@hotmail.es>
2026-02-12 14:09:40 +0100

    Update basic Web with JS

--- 5.JS/2_web/main.js
+++ 5.JS/2_web/main.js
@@ -1,82 +1,33 @@
-//  document.addEventListener('DOMContentLoaded', function () {
-      const toggle = document.querySelector('#theme-toggle');
-      toggle.addEventListener('change', function () {
+export let theme = "light";
 
-        if (this.checked) {
-          document.body.classList.add('dark');
-        } else {
-          document.body.classList.remove('dark');
-        }
-      });
-//     });
-
-const user = {
-    name: "Pepe",
-    address: {
-        street: "",
-    },
-    friends: [
-        {
-            name: "Juan",
-            address: {
-                street: "",
-            },
-        },
-            {
-            name: "Rosa",
-            address: {
-                street: "",
-            },
-        },
-    ],
+const handleChange = (event) => {
+    const element = event.target;
+    theme = element.checked ? "dark" : "light";
+    console.log(theme);
 };
 
-console.log("Loaded main");
-console.log(user)
-console.dir(document);
-console.log('ChildNodes', document.childNodes)
-console.log('Children', document.children)
+const handleDialogMenu = (event) => {
+    console.log("Click", event);
+    const menuDialogElement = document.querySelector("#menu-dialog");
+    if (event.target.localName === "svg") {
+        event.preventDefault();
+        menuDialogElement.showModal();
+    } else {
+        menuDialogElement.close();
+    }
+};
 
-const htmlElement = document.children[0]
-console.dir(htmlElement)
-console.log('ChildNodes', htmlElement.childNodes)
-console.log('Children', htmlElement.children)
-
-console.log('Children', document.body.children)
-
-const link1 = document.body.children[2].children[0].children[0].children[0]
-console.log(link1)
-link1.previousElementSibling
-link1.nextElementSibling
-link1.parentElement
-link1.href = "https://github.com/alce65"
-
-// Accesos directos al dom
-
-// document.getElementById('x')
-// document.getElementsByClassName('foote')
-// document.getElementsByTagName('li')
-// document.getElementsByName()
-
-const home = document.querySelector('#home')
-document.querySelector('.home')
-document.querySelector('[aria-label=home]')
-//document.querySelector("main>section::nth-of-type(1)")
-const p = home.querySelector('p')
-const hoy = new Date()
-p.textContent = 'Aprendiendo a manipular el DOM ' + hoy
-
-const newP = document.createElement('p')
-newP.textContent = "Hola Amigos"
-console.dir(newP)
-home.appendChild(newP)
-
-const user2 = 'Pepe'
-
-home.innerHTML +=
-`
-    <ul>
-        <li>Horror ${user2}</li>
-        <li>Horror</li>
-    </ul> 
-`
+export function main() {
+    console.log("Loaded main");
+    // Theme
+    const toggleElement = document.querySelector("#theme-toggle");
+    toggleElement.addEventListener("change", handleChange);
+    
+    // Menu (Navegación)
+    const menuIconElement = document.querySelector("#menu-icon");
+    const closeDialogElement = document.querySelector("#menu-dialog button");
+    const menuDialogElement = document.querySelector("#menu-dialog menu");
+    menuIconElement.addEventListener("click", handleDialogMenu);
+    closeDialogElement.addEventListener("click", handleDialogMenu);
+    menuDialogElement.addEventListener("click", handleDialogMenu);
+}
